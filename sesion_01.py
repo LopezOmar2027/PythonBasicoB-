3004706cbaad217d244e124b4057fa7982cbb02c
LopezOmar2027 <omar.universe16@gmail.com>
2026-02-12 18:37:08 -0800

    guardando mi sesion de Python Basico

---
+++ sesion_01.py
@@ -0,0 +1,14 @@
+# numerico y flotante
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7.7))
+print(int(1+2))
+print(int(10*2))
+print("=====Operadores Mtematicos=====")
+#+
+#-
+#*
+#/
+#**
+#% Modulo
