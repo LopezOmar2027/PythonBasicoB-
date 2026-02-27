16e75f9eb9c35176150d229bd5ba1f14cc8c5854
LopezOmar2027 <omar.universe16@gmail.com>
2026-02-26 19:20:31 -0800

    guarde mi sesion de main.py

--- oop/main.py
+++ oop/main.py
@@ -2,10 +2,10 @@ from Enemigo import*
 from Zombie import*
 from Ogro import*
 
-Zombie = zombie(10, 1)
-Ogro =Ogro(20, 3)
-print(f"{Zombie.get_tipo_enemigo()}tiene {zombie.puntos_energia}de energia y puede hacer ataques de {zombie.ataque}")
+Zombie =zombie(10, 1)
+ogro =Ogro(20, 3)
+print(f"{zombie.get_tipo_enemigo()}tiene {zombie.puntos_energia}de energia y puede hacer ataques de {zombie.ataque}")
 print(f"{zombie.habla()}")
-print(f"{Ogro.get_tipo_enemigo()}tiene {Ogro.puntos_energia}de energia y puede hacer ataques de {Ogro.ataque}")
-print(f"{Ogro.habla()}")
+print(f"{ogro.get_tipo_enemigo()}tiene {ogro.puntos_energia}de energia y puede hacer ataques de {ogro.ataque}")
+print(f"{ogro.habla()}")
 
