57d3057bef3fd2f192b433376579f8657a26f3ab
LopezOmar2027 <omar.universe16@gmail.com>
2026-02-26 18:47:13 -0800

    realizamos un ogro en documentos

---
+++ oop/Ogro.py
@@ -0,0 +1,9 @@
+from Enemigo import *
+
+class Ogro(enemigo):
+    def _init_(self, puntos_energia=20, ataque=3):
+        super()._init_(tipo_enemigo='Ogro',puntos_energia=puntos_energia,ataque=ataque)
+
+        def habla(self):
+            print("Ogro aplasta todo!!!")
+            
